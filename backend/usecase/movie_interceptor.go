d074f035cc8a27f81793f23e32d252fee2b920db
suhaya3317 <hayato_17n1200071@nnn.ed.jp>
2019-02-26 09:39:43 +0900

    usecase

---
+++ backend/usecase/movie_interceptor.go
@@ -0,0 +1,30 @@
+package usecase
+
+import (
+	"context"
+	"net/http"
+)
+
+type MovieMuxInterceptor struct {
+	MovieMuxRepository MovieMuxRepository
+}
+
+func (interceptor *MovieMuxInterceptor) Get(r *http.Request, key string) string {
+	return interceptor.MovieMuxRepository.Find(r, key)
+}
+
+type MovieAPIInterceptor struct {
+	MovieAPIRepository MovieAPIRepository
+}
+
+func (interceptor *MovieAPIInterceptor) GetPopularMovies(ctx context.Context, page string) (*http.Response, error) {
+	return interceptor.MovieAPIRepository.FindAll(ctx, page)
+}
+
+type LogInterceptor struct {
+	LogRepository LogRepository
+}
+
+func (interceptor *LogInterceptor) LogInfo(ctx context.Context, format string, args interface{}) {
+	interceptor.LogRepository.Output(ctx, format, args)
+}
