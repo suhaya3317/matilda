da88c85e531c17d86fa0f64ae002c512a38ae3a8
suhaya3317 <hayato_17n1200071@nnn.ed.jp>
2019-02-26 11:28:38 +0900

    domain

--- backend/domain/movie.go
+++ backend/domain/movie.go
@@ -1,6 +1,6 @@
 package domain
 
-type Movies struct {
+type MoviesAPI struct {
 	Page         int       `json:"page"`
 	TotalResults int       `json:"total_results"`
 	TotalPages   int       `json:"total_pages"`
@@ -24,8 +24,65 @@ type Results struct {
 	ReleaseDate      string  `json:"release_date"`
 }
 
-type MatildaMovie struct {
+type Movie struct {
 	ID         int    `json:"id"`
 	Title      string `json:"title"`
 	PosterPath string `json:"poster_path"`
+}
+
+type MovieAPI struct {
+	Adult               bool                  `json:"adult"`
+	BackdropPath        string                `json:"backdrop_path"`
+	BelongsToCollection BelongsToCollection   `json:"belongs_to_collection"`
+	Budget              int                   `json:"budget"`
+	Genres              []Genres              `json:"genres"`
+	Homepage            string                `json:"homepage"`
+	ID                  int                   `json:"id"`
+	ImdbId              string                `json:"imdb_id"`
+	OriginalLanguage    string                `json:"original_language"`
+	OriginalTitle       string                `json:"original_title"`
+	Overview            string                `json:"overview"`
+	Popularity          float64               `json:"popularity"`
+	PosterPath          string                `json:"poster_path"`
+	ProductionCompanies []ProductionCompanies `json:"production_companies"`
+	ProductionCountries []ProductionCountries `json:"production_countries"`
+	ReleaseDate         string                `json:"release_date"`
+	Revenue             int                   `json:"revenue"`
+	Runtime             int                   `json:"runtime"`
+	SpokenLanguages     []SpokenLanguages     `json:"spoken_languages"`
+	Status              string                `json:"status"`
+	Tagline             string                `json:"tagline"`
+	Title               string                `json:"title"`
+	Video               bool                  `json:"video"`
+	VoteAverage         float64               `json:"vote_average"`
+	VoteCount           int                   `json:"vote_count"`
+}
+
+type BelongsToCollection struct {
+	ID           int    `json:"id"`
+	Name         string `json:"name"`
+	PosterPath   string `json:"poster_path"`
+	BackdropPath string `json:"backdrop_path"`
+}
+
+type Genres struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+type ProductionCompanies struct {
+	ID            int    `json:"id"`
+	LogoPath      string `json:"logo_path"`
+	Name          string `json:"name"`
+	OriginCountry string `json:"origin_country"`
+}
+
+type ProductionCountries struct {
+	Iso_3166_1 string `json:"iso_3166_1"`
+	Name       string `json:"name"`
+}
+
+type SpokenLanguages struct {
+	Iso_639_1 string `json:"iso_639_1"`
+	Name      string `json:"name"`
 }
