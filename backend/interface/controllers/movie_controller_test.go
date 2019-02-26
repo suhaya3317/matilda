f7448a6ad63353547dba3342a9ad360336821c4a
suhaya3317 <hayato_17n1200071@nnn.ed.jp>
2019-02-26 11:31:06 +0900

    test

--- backend/interface/controllers/movie_controller_test.go
+++ backend/interface/controllers/movie_controller_test.go
@@ -49,8 +49,8 @@ func (mux *MockMovieMuxRepository) Find(r *http.Request, key string) string {
 }
 
 func (movie *MockMovieAPIRepository) FindAll(ctx context.Context, page string) (*http.Response, error) {
-	var movies domain.Movies
-	movies = domain.Movies{
+	var moviesAPI domain.MoviesAPI
+	moviesAPI = domain.MoviesAPI{
 		Page:         1,
 		TotalResults: 19801,
 		TotalPages:   991,
@@ -73,7 +73,90 @@ func (movie *MockMovieAPIRepository) FindAll(ctx context.Context, page string) (
 			},
 		},
 	}
-	body, err := json.Marshal(movies)
+	body, err := json.Marshal(moviesAPI)
+	if err != nil {
+		return nil, err
+	}
+
+	res := &http.Response{
+		StatusCode: http.StatusOK,
+		Body:       ioutil.NopCloser(bytes.NewReader(body)),
+	}
+	return res, nil
+}
+
+func (movie *MockMovieAPIRepository) Find(cxt context.Context, id string) (*http.Response, error) {
+	var movieAPI domain.MovieAPI
+	movieAPI = domain.MovieAPI{
+		Adult:        false,
+		BackdropPath: "/aQXTw3wIWuFMy0beXRiZ1xVKtcf.jpg",
+		BelongsToCollection: domain.BelongsToCollection{
+			ID:           0,
+			Name:         "",
+			PosterPath:   "",
+			BackdropPath: "",
+		},
+		Budget: 170000000,
+		Genres: []domain.Genres{
+			{
+				ID:   28,
+				Name: "Action",
+			},
+			{
+				ID:   878,
+				Name: "Science Fiction",
+			},
+			{
+				ID:   53,
+				Name: "Thriller",
+			},
+		},
+		Homepage:         "https://www.foxmovies.com/movies/alita-battle-angel",
+		ID:               399579,
+		ImdbId:           "tt0437086",
+		OriginalLanguage: "en",
+		OriginalTitle:    "Alita: Battle Angel",
+		Overview:         "When Alita awakens with no memory of who she is in a future world she does not recognize, she is taken in by Ido, a compassionate doctor who realizes that somewhere in this abandoned cyborg shell is the heart and soul of a young woman with an extraordinary past.",
+		Popularity:       492.344,
+		PosterPath:       "/xRWht48C2V8XNfzvPehyClOvDni.jpg",
+		ProductionCompanies: []domain.ProductionCompanies{
+			{
+				ID:            10807,
+				LogoPath:      "/j0BcMaJKIiDDYHq9lriTcM0Npka.png",
+				Name:          "Troublemaker Studios",
+				OriginCountry: "US",
+			},
+			{
+				ID:            574,
+				LogoPath:      "/iB6GjNVHs5hOqcEYt2rcjBqIjki.png",
+				Name:          "Lightstorm Entertainment",
+				OriginCountry: "US",
+			},
+		},
+		ProductionCountries: []domain.ProductionCountries{
+			{
+				Iso_3166_1: "AR",
+				Name:       "Argentina",
+			},
+		},
+		ReleaseDate: "2019-01-31",
+		Revenue:     263358391,
+		Runtime:     122,
+		SpokenLanguages: []domain.SpokenLanguages{
+			{
+				Iso_639_1: "en",
+				Name:      "English",
+			},
+		},
+		Status:      "Released",
+		Tagline:     "An angel falls. A warrior rises.",
+		Title:       "Alita: Battle Angel",
+		Video:       false,
+		VoteAverage: 6.9,
+		VoteCount:   891,
+	}
+
+	body, err := json.Marshal(movieAPI)
 	if err != nil {
 		return nil, err
 	}
@@ -118,6 +201,43 @@ func TestMovieController_GetMovies(t *testing.T) {
 
 	actual := string(body)
 	expected := `[{"id":399579,"title":"Alita: Battle Angel","poster_path":"https://image.tmdb.org/t/p/w300_and_h450_bestv2/xRWht48C2V8XNfzvPehyClOvDni.jpg"}]`
+
+	err = IsEqualJSON(actual, expected)
+	if err != nil {
+		t.Fatalf("Failed to IsEqualJson: %v", err)
+	}
+}
+
+func TestMovieController_GetMovie(t *testing.T) {
+	inst, err := aetest.NewInstance(nil)
+	if err != nil {
+		t.Fatalf("Failed to create instance: %v", err)
+	}
+	defer inst.Close()
+
+	req, err := inst.NewRequest("GET", "/api/v1/movies/399579", nil)
+	if err != nil {
+		t.Fatalf("Failed to create req: %v", err)
+	}
+
+	res := httptest.NewRecorder()
+
+	apErr := target.GetMovie(res, req)
+	if apErr != nil {
+		t.Fatalf("GetMovie error: %v", apErr)
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Fatalf("Failed to read res.Body: %v", err)
+	}
+
+	if res.Code != 200 {
+		t.Fatalf("StatusCode: %v, Response.Body: %v", res.Code, body)
+	}
+
+	actual := string(body)
+	expected := `{"id":399579,"title":"Alita: Battle Angel","poster_path":"https://image.tmdb.org/t/p/w300_and_h450_bestv2/xRWht48C2V8XNfzvPehyClOvDni.jpg"}`
 
 	err = IsEqualJSON(actual, expected)
 	if err != nil {
